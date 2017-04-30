66cafb057818ff873bc7fba5085bbc71416dbffd
Lucas Selani <lucas_selani@hotmail.com>
2017-04-30 17:00:55 -0300

    tirando permissoes de camera

--- app/src/main/java/br/sw/cacadoresdelivrosbr/view/activities/LoginActivity.java
+++ app/src/main/java/br/sw/cacadoresdelivrosbr/view/activities/LoginActivity.java
@@ -128,11 +128,10 @@ public class LoginActivity extends FragmentActivity {
     public boolean askPermissions() {
         Log.v("Main", "AskingPermissions");
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
             ActivityCompat.requestPermissions(this, permissions, 3030);
             Log.v("Main", "InsideIf");
